5270d64f381098209c670fe18f0e3cb0caa62230
hocgin <hocgin@gmail.com>
2019-07-30 22:56:35 +0800

    devtools 初稿

--- mybatis-squirrel-plugins/mybatis-squirrel-devtools/src/main/java/in/hocg/squirrel/devtools/DevtoolsAutoConfiguration.java
+++ mybatis-squirrel-plugins/mybatis-squirrel-devtools/src/main/java/in/hocg/squirrel/devtools/DevtoolsAutoConfiguration.java
@@ -1,18 +1,19 @@
 package in.hocg.squirrel.devtools;
 
-import in.hocg.squirrel.devtools.reload.MybatisMapperReload;
-import in.hocg.squirrel.devtools.sql.MybatisWatchSql;
+import in.hocg.squirrel.devtools.reload.WatchDog;
+import in.hocg.squirrel.devtools.sql.WatchSqlInterceptor;
 import in.hocg.squirrel.spring.boot.autoconfigure.MybatisAutoConfiguration;
+import in.hocg.squirrel.spring.boot.autoconfigure.MybatisProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 /**
  * Created by hocgin on 2019-07-30.
@@ -20,28 +21,26 @@ import org.springframework.context.annotation.Configuration;
  *
  * @author hocgin
  */
+@Slf4j
 @Configuration
 @EnableConfigurationProperties(DevtoolsProperties.class)
 @AutoConfigureAfter({MybatisAutoConfiguration.class})
 public class DevtoolsAutoConfiguration implements InitializingBean,
         ApplicationContextAware {
     private ApplicationContext applicationContext;
-    private SqlSessionFactory sqlSessionFactory;
-    
-    @Bean
-    @ConditionalOnMissingBean
-    public MybatisMapperReload mybatisAutoConfiguration() {
-        return new MybatisMapperReload(applicationContext);
-    }
-    @Bean
-    @ConditionalOnMissingBean
-    public MybatisWatchSql mybatisWatchSql() {
-        return new MybatisWatchSql(applicationContext);
-    }
-    
+ 
     @Override
     public void afterPropertiesSet() throws Exception {
-        this.sqlSessionFactory = applicationContext.getBean(SqlSessionFactory.class);
+        SqlSessionFactory sqlSessionFactory = applicationContext.getBean(SqlSessionFactory.class);
+        MybatisProperties mybatisProperties = applicationContext.getBean(MybatisProperties.class);
+        
+        log.debug("==========[[启动监听 SQL]]==========");
+        sqlSessionFactory.getConfiguration().addInterceptor(new WatchSqlInterceptor());
+    
+        log.debug("==========[[启动 Mapper 监听🐕]]==========");
+        Resource[] mapperResources = mybatisProperties.resolveMapperLocations();
+        new Thread(() -> new WatchDog(mapperResources, sqlSessionFactory, true), "MybatisMapperReload").start();
+        
     }
     
     @Override
