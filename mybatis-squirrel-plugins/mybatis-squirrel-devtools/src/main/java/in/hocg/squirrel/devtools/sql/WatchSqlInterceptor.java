1e1e65a813591718f7b67aadb5e00f4c89bb9b58
hocgin <hocgin@gmail.com>
2019-07-31 20:39:09 +0800

    devtools 初稿

--- mybatis-squirrel-plugins/mybatis-squirrel-devtools/src/main/java/in/hocg/squirrel/devtools/sql/WatchSqlInterceptor.java
+++ mybatis-squirrel-plugins/mybatis-squirrel-devtools/src/main/java/in/hocg/squirrel/devtools/sql/WatchSqlInterceptor.java
@@ -19,11 +19,11 @@ import org.apache.ibatis.reflection.MetaObject;
 import org.apache.ibatis.reflection.SystemMetaObject;
 import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.ResultHandler;
-import org.apache.ibatis.type.JdbcType;
 
-import java.lang.reflect.Proxy;
-import java.sql.PreparedStatement;
 import java.sql.Statement;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -59,50 +59,49 @@ public class WatchSqlInterceptor extends AbstractInterceptor {
     
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
-        Object[] args = invocation.getArgs();
-        PreparedStatement statement = getStatement(args[0]);
         
-        
+        // 计算耗时
         Stopwatch stopwatch = Stopwatch.createStarted();
         Object result = invocation.proceed();
         stopwatch.stop();
-    
+        
         StatementHandler target = getTarget(invocation.getTarget());
         MetaObject metaObject = SystemMetaObject.forObject(target);
         MappedStatement mappedStatement = (MappedStatement) metaObject.getValue(StatementHandlerFields.DELEGATE__MAPPED_STATEMENT);
         BoundSql boundSql = target.getBoundSql();
         String originalSql = Pretty.sql(boundSql);
         Configuration configuration = mappedStatement.getConfiguration();
-    
+        
         List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
         Object parameterObject = boundSql.getParameterObject();
         List<Object> sqlArgs = new ArrayList<>();
         if (parameterMappings != null) {
             MetaObject parameterMetaObject = parameterObject == null ? null : configuration.newMetaObject(parameterObject);
-            for (int i = 0; i < parameterMappings.size(); i++) {
-                ParameterMapping parameterMapping = parameterMappings.get(i);
+            for (ParameterMapping parameterMapping : parameterMappings) {
                 if (parameterMapping.getMode() != ParameterMode.OUT) {
                     String propertyName = parameterMapping.getProperty();
                     Object value;
-                    if (boundSql.hasAdditionalParameter(propertyName)) { // issue #448 ask first for additional params
+                    if (boundSql.hasAdditionalParameter(propertyName)) {
                         value = boundSql.getAdditionalParameter(propertyName);
                     } else {
                         value = parameterMetaObject == null ? null : parameterMetaObject.getValue(propertyName);
                     }
-                    JdbcType jdbcType = parameterMapping.getJdbcType();
-                    // .. 处理参数设值
-                    if (value instanceof String) {
+            
+                    // 处理参数
+                    if (value instanceof String
+                            || value instanceof LocalDateTime
+                            || value instanceof LocalDate
+                            || value instanceof LocalTime) {
                         sqlArgs.add(String.format("'%s'", value));
                     } else {
                         sqlArgs.add(Objects.toString(value));
                     }
-                    log.debug("{}: {}({})", i, value, jdbcType);
                 }
             }
         }
-    
+        
         String sql = TextFormatter.arrayFormat(originalSql, sqlArgs.toArray(), TextFormatter.SQL_PLACEHOLDER);
-    
+        
         StringJoiner stringJoiner = new StringJoiner(System.lineSeparator())
                 .add("")
                 .add("========================================================================================")
@@ -114,12 +113,4 @@ public class WatchSqlInterceptor extends AbstractInterceptor {
         return result;
     }
     
-    
-    private <T> T getStatement(Object statement) {
-        if (Proxy.isProxyClass(statement.getClass())) {
-            MetaObject metaObject = SystemMetaObject.forObject(statement);
-            return getTarget(metaObject.getValue("h.statement"));
-        }
-        return (T) statement;
-    }
 }
