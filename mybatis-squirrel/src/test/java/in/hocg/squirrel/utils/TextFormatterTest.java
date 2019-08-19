0297c719f09f3de1bd0b2aae94cc35770bfd46c5
hocgin <hocgin@gmail.com>
2019-08-19 14:12:13 +0800

    # 修复BUG 补充单元测试

--- mybatis-squirrel/src/test/java/in/hocg/squirrel/utils/TextFormatterTest.java
+++ mybatis-squirrel/src/test/java/in/hocg/squirrel/utils/TextFormatterTest.java
@@ -18,5 +18,6 @@ class TextFormatterTest {
         Assertions.assertEquals("P1} {}", TextFormatter.format("P{i} {}", "1}"));
         Assertions.assertEquals("红颜色的苹果很甜，但是它有毒", TextFormatter.format("{}颜色的苹果很甜，但是它{}", "红","有毒"));
         Assertions.assertEquals("null颜色的苹果很甜，但是它{}", TextFormatter.format("{}颜色的苹果很甜，但是它{}", null));
+        Assertions.assertEquals("红颜色的苹果很甜，但是它有毒", TextFormatter.format("{}颜色的苹果很甜，但是它{}", new String[]{"红","有毒"}));
     }
 }
