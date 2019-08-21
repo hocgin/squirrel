03588f7cc93d2f86273d455303d5f218c8cb2d91
hocgin <hocgin@gmail.com>
2019-08-21 23:49:44 +0800

    # Cashew 初步结构

---
+++ mybatis-squirrel-plugins/mybatis-squirrel-cashew/src/main/java/in/hocg/squirrel/cashew/CashewInterceptor.java
@@ -0,0 +1,17 @@
+package in.hocg.squirrel.cashew;
+
+import in.hocg.squirrel.intercepts.AbstractInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.plugin.Intercepts;
+
+/**
+ * Created by hocgin on 2019-08-21.
+ * email: hocgin@gmail.com
+ *
+ * @author hocgin
+ */
+@Slf4j
+@Intercepts({})
+public class CashewInterceptor extends AbstractInterceptor {
+
+}
